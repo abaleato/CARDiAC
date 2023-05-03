3f499f72f5145b925314c04066729f6964195df5
Anton Baleato <ab2368@cam.ac.uk>
2023-05-03 16:39:13 -0700

    parallel version now working

--- scripts/compute_biases_in_parallel.py
+++ scripts/compute_biases_in_parallel.py
@@ -1,13 +1,17 @@
-
+#!/global/homes/a/ab2368/.conda/envs/varying_dndz python
+import sys
+sys.path.insert(0, '/global/homes/a/ab2368/varying_dndzs/Varying_dndzs/code/')
 import numpy as np
 import biases
 
 # Load scenario from file
 sigma_zshift = 0.02703
-filename = '../data/redmagic_wn_sigmazshift{}'.format(sigma_zshift)
+nside = 8
+filename = '/pscratch/sd/a/ab2368/data/redmagic_wn_sigmazshift{}_nside'.format(sigma_zshift, nside)
 loaded_exp = biases.load_from_file(filename)
 
 # Hyperparameters for numerical integration
+num_processes = 16
 miniter = 1000
 maxiter = 5000
 tol = 1e-11
@@ -18,9 +22,11 @@ loaded_exp.ells = np.linspace(10, 1000, 16, dtype=int)
 if hasattr(loaded_exp, 'conv_bias'):
     print("It looks like you've already computed the biases for this scenario. Please delete them from file you really want to re-rerun things.")
 else:
-    loaded_exp.conv_bias = biases.mode_coupling_bias(redmagic_wn, loaded_exp.ells, parallelize=True, miniter=miniter, maxiter=maxiter, tol=tol)
-    loaded_exp.additive_bias = biases.additive_bias(redmagic_wn, loaded_exp.ells, parallelize=True, miniter=miniter, maxiter=maxiter, tol=tol)
-    loaded_exp.unbiased_clgg = biases.unbiased_term(redmagic_wn, loaded_exp.ells, parallelize=True, miniter=miniter, maxiter=maxiter, tol=tol)
+    loaded_exp.conv_bias = biases.mode_coupling_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+    loaded_exp.save_properties(filename)
+    loaded_exp.additive_bias = biases.additive_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+    loaded_exp.save_properties(filename)
+    loaded_exp.unbiased_clgg = biases.unbiased_term(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
 
 # Save to file, this time including the biases as an attribute!
 loaded_exp.save_properties(filename)
