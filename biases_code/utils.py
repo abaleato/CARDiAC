2f4f951981cf4cda2f00bc37d14097e1dfe753df
Antón Baleato Lizancos <ab2368@cam.ac.uk>
2023-05-08 22:04:43 -0700

    new function to generate templates

--- biases_code/utils.py
+++ biases_code/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import healpy as hp
 
 def find_closest_indices(chi_array, ref_array):
     overlapping = np.logical_and(ref_array > np.min(chi_array), ref_array < np.max(chi_array))
@@ -14,3 +15,24 @@ def bl(fwhm_arcmin, lmax):
     """
     ls = np.arange(0, lmax + 1)
     return np.exp(-(fwhm_arcmin * np.pi / 180. / 60.) ** 2 / (16. * np.log(2.)) * ls * (ls + 1.))
+
+def generate_template(nside, alpha, target_std, lmax_pert):
+    '''
+    Generate a pixelized map where each pixel value corresponds to a random shift of the
+    center of the dndz. The map has a standard deviation set by target_std, and some underlying
+    power-law power spectrum for the shifts
+    Inputs:
+        * nside = int. Defines the pixelization
+        * alpha = float. Power law index for the perturbation Cls
+        * target_std = float. Standard deviation of the resulting map
+        * lmax_pert = int. lmax of the Cls
+    '''
+    pert_cls = np.arange(lmax_pert, dtype=float)**alpha
+    pert_cls[0]=0
+
+    # Convert to a map
+    sim_map = hp.synfast(pert_cls, nside)
+
+    # Normalize Cls to give desired variance
+    norm_factor_for_alms = np.nan_to_num(target_std**2 / np.var(sim_map, ddof=1))**0.5
+    return norm_factor_for_alms*sim_map
