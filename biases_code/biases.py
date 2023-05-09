02862286f336f10d9336f5ca8b5e53e959c754cf
Antón Baleato Lizancos <ab2368@cam.ac.uk>
2023-05-09 08:49:55 -0700

    convenience function to calculate all contributions

--- biases_code/biases.py
+++ biases_code/biases.py
@@ -551,6 +551,43 @@ def unbiased_term_at_l(exp, miniter, maxiter, tol, l):
                                          args=(exp.phi_fid, Pkgg_interp_1D), miniter=miniter, maxiter=maxiter, tol=tol)
     return result
 
+def compute_contributions(filename, ells=None, num_processes = 1, miniter = 1000, maxiter = 5000, tol = 1e-11,
+                          lprime_max = 'none'):
+    '''
+    A convenience function to calculate all the contributions
+    - Inputs:
+    * filename = str. The filename of the experiment object to load and compute from.
+    * ells (optional) = np.array of ints. Multipoles at which to evauate the contributions
+    * num_processes (optional) = int. Number of processes to use. If >1, use multirocessing
+    * miniter (optional) = int. Minimum number of iterations for quadrature.
+    * maxiter (optional) = int. Maximum number of iterations for quadrature.
+    * tol (optional) = int. Error tolerance before breaking numerical integration.
+    * lprime_max (optional) = int. Value of l above which we ignore the anisotropy in C_l^{\Delta \phi}
+    '''
+    if ells is None:
+        # The ells where we want to evaluate the spectra
+        ells = np.logspace(np.log10(50), np.log10(1500), 48, dtype=int)
+
+    loaded_exp = load_from_file(filename)
+    loaded_exp.ells = ells
+    unbiased_clgg = unbiased_term(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+    loaded_exp.unbiased_clgg = unbiased_clgg
+
+    # Compute full mode-coupling bias
+    loaded_exp.conv_bias = mode_coupling_bias(loaded_exp, loaded_exp.ells, lprime_max=lprime_max, num_processes=num_processes,
+                                                     miniter=miniter, maxiter=maxiter, tol=tol)
+    # Compute analytic approximations to mode-coupling bias
+    loaded_exp.analytic_mcbias_toy_model = mode_coupling_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes,
+                                                                     miniter=miniter, maxiter=maxiter, tol=tol, mode='analytic_toy_model')
+    loaded_exp.analytic_mcbias_via_variance = mode_coupling_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes,
+                                                                        miniter=miniter, maxiter=maxiter, tol=tol, mode='analytic_via_variance')
+
+    # Compute additive bias
+    loaded_exp.additive_bias = additive_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+
+    # Save to file, this time including the biases as an attribute!
+    loaded_exp.save_properties(filename)
+    return loaded_exp
 #
 # Loading an experiment from file
 #
