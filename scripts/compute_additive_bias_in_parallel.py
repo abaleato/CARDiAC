95b96192b44b55ddfbee5257eee8a0a146b8474f
Anton Baleato <ab2368@cam.ac.uk>
2023-05-08 19:52:35 -0700

    polished scripts

--- scripts/compute_additive_bias_in_parallel.py
+++ scripts/compute_additive_bias_in_parallel.py
@@ -1,29 +1,31 @@
 #!/global/homes/a/ab2368/.conda/envs/varying_dndz python
 import sys
-sys.path.insert(0, '/global/homes/a/ab2368/varying_dndzs/Varying_dndzs/code/')
+sys.path.insert(0, '/global/homes/a/ab2368/varying_dndzs/Varying_dndzs/biases_code/')
 import numpy as np
 import biases
 
 # Load scenario from file
-sigma_zshift = 0.02703
-nside = 8
-filename = '/pscratch/sd/a/ab2368/data/redmagic_wn_sigmazshift{}_nside'.format(sigma_zshift, nside)
-loaded_exp = biases.load_from_file(filename)
+sigma_zshifts = np.array([0.1])*0.02703 #np.array([2, 1, 0.5, 0.1])*0.02703 #np.array([0.02703])
+nsides = np.array([32])#np.array([8, 16, 32])
 
 # Hyperparameters for numerical integration
-num_processes = 16
+num_processes = 1
 miniter = 1000
 maxiter = 5000
 tol = 1e-11
 
-# The ells where we want to evaluate the spectra
-loaded_exp.ells = np.logspace(0, np.log10(200), 16, dtype=int)
+for sigma_zshift in sigma_zshifts:
+    for nside in nsides:
+        filename = '/pscratch/sd/a/ab2368/data/redmagic_wn_NOSMOOTHING_sigmazshift{}_nside'.format(sigma_zshift, nside)
+        loaded_exp = biases.load_from_file(filename)
 
-if hasattr(loaded_exp, 'additive_bias'):
-    print("It looks like you've already computed the bias for this scenario. Please delete it from file you really want to re-rerun things.")
-else:
-    loaded_exp.additive_bias = biases.additive_bias(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
-    #loaded_exp.unbiased_clgg = biases.unbiased_term(loaded_exp, loaded_exp.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+        # The ells where we want to evaluate the spectra
+        loaded_exp.additive_ells = np.logspace(0, np.log10(200), 16, dtype=int)
 
-# Save to file, this time including the biases as an attribute!
-loaded_exp.save_properties(filename)
+        if hasattr(loaded_exp, 'additive_bias'):
+            print("It looks like you've already computed the bias for this scenario. Please delete it from file you really want to re-rerun things.")
+        else:
+            loaded_exp.additive_bias = biases.additive_bias(loaded_exp, additive_ells.ells, num_processes=num_processes, miniter=miniter, maxiter=maxiter, tol=tol)
+
+        # Save to file, this time including the biases as an attribute!
+        loaded_exp.save_properties(filename)
