5f4f0b01e31566e90c8216949f70162e09a8740d
Antón Baleato Lizancos <ab2368@cam.ac.uk>
2023-05-16 16:07:17 -0700

    bugfix

--- new_code/fields.py
+++ new_code/fields.py
@@ -13,7 +13,7 @@ class grid:
         self.n_samples_of_chi = n_samples_of_chi
         self.chi_min_int = Planck18.comoving_distance(z_min_int).value
         self.chi_max_int = Planck18.comoving_distance(z_max_int).value
-        self.chi_array = np.linspace(chi_min_int, chi_max_int + 100, n_samples_of_chi)
+        self.chi_array = np.linspace(self.chi_min_int, self.chi_max_int + 100, n_samples_of_chi)
         # Get the redshift corresponding to these values of chi in the Planck18 cosmology
         self.z_array = np.zeros((hp.nside2npix(nside), n_samples_of_chi))
         for i, chi in enumerate(self.chi_array):
