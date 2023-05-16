3692ea7bfa97f55c918f0365a6b8dcb0bc4f89f6
Antón Baleato Lizancos <ab2368@cam.ac.uk>
2023-05-16 16:01:34 -0700

    preliminary field functionality

--- new_code/fields.py
+++ new_code/fields.py
@@ -1,47 +1,101 @@
 import numpy as np
 import healpy as hp
-import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy.integrate import quadrature
+from astropy.cosmology import Planck18, z_at_value
+from astropy.constants import c
+import astropy.units as u
+import utils
+
+class grid:
+    def __init__(self, nside, n_samples_of_chi, chi_min_int, chi_max_int):
+        self.nside = nside
+        self.n_samples_of_chi = n_samples_of_chi
+        self.chi_max_int = chi_max_int
+        self.chi_min_int = chi_min_int
+        self.chi_array = np.linspace(chi_min_int, chi_max_int + 100, n_samples_of_chi)
+        # Get the redshift corresponding to these values of chi in the Planck18 cosmology
+        self.z_array = np.zeros((hp.nside2npix(nside), n_samples_of_chi))
+        for i, chi in enumerate(self.chi_array):
+            self.z_array[:, i] = z_at_value(Planck18.comoving_distance, chi * u.Mpc)
+
+    def hashdict(self):
+        return {'n_samples_of_chi': self.n_samples_of_chi,
+                'nside': self.nside, 'chi_min_int': self.chi_min_int, 'chi_max_int': self.chi_max_int}
+
+    def __eq__(self, other):
+        return self.compatible(other)
+
+    def compatible(self, other):
+        return ((self.n_samples_of_chi == other.n_samples_of_chi) and
+                (self.nside == other.nside)  and
+                (self.chi_min_int == other.chi_min_int) and
+                (self.chi_max_int == other.chi_max_int))
 
 class Field:
-    def __init__(self, delta_p_maps, kernel):
-        """ Contains information about spatial shifts to the center or width of a dndz """
-        
-        # Take the spherical harmonic transform of each r slice. Conveniently, we can take complex SHT so array sizes reduce by x2
-        delta_p_lm_of_chi = np.zeros((hp.Alm.getsize(3 * template.nside - 1), n_samples_of_chi), dtype=complex)
-        for i in range(n_samples_of_chi):
-            delta_p_lm_of_chi[:, i] = hp.map2alm(delta_p_maps[:, i])
-        
+    def __init__(self, grid, p_pert_array, p_fid_array):
 
+        self.delta_p_maps = p_pert_array - p_fid_array
+        self.kernel = interp1d(grid.chi_array, p_fid_array)
+        self.grid = grid
+
+        # Take the spherical harmonic transform of each chi slice.
+        # Conveniently, we can take complex SHT so array sizes reduce by x2
+        self.delta_p_lm_of_chi = np.zeros((hp.Alm.getsize(3 * grid.nside - 1), grid.n_samples_of_chi), dtype=complex)
+        for i in range(grid.n_samples_of_chi):
+            self.delta_p_lm_of_chi[:, i] = hp.map2alm(self.delta_p_maps[:, i])
 
 class gal_delta(Field):
-    def __init__(self, Template):
-        """ Generate a pixelized map where each pixel value corresponds to a random shift of the
-            - Inputs:
-                * sigma_of_shift = float. Standard deviation of the shifts across the template
+    def __init__(self, grid, sigma, z_mean, template_zmean_shifts=0., template_width_shifts=0., get_delta_p=True):
+        self.sigma = sigma
+        self.z_mean = z_mean
 
-        """
-        kernel = 
+        # Convert template of z-shifts to chi-shifts
+        chimean_shifts_array = Planck18.comoving_distance(z_mean + template_zmean_shifts).value \
+                               - Planck18.comoving_distance(z_mean).value
+        width_shifts_array = Planck18.comoving_distance(z_mean + template_width_shifts).value \
+                               - Planck18.comoving_distance(z_mean).value
 
-        super().__init__()
-        
-        
+        # In each pixel, calculate the perturbed dndz as a Gaussian in chi
+        dndz_perturbed = (1 / ((grid.chi_sigma_fid + width_shifts_array[..., np.newaxis]) * np.sqrt(2 * np.pi))) * np.exp(
+            -(grid.chi_array - grid.chi_mean_fid - chimean_shifts_array[..., np.newaxis]) ** 2 / (
+                        2 * (grid.chi_sigma_fid + width_shifts_array[..., np.newaxis]) ** 2))
+        # Take the fiducial dndz to be the monopole of the perturbed dndz
+        dndz_fid = np.mean(dndz_perturbed, axis=0)
+
+        # Convert dndz to selection function
+        phi_perturbed_array = (Planck18.H(grid.z_array[0, :]) / c).value * dndz_perturbed
+        phi_fid_array = (Planck18.H(grid.z_array[0, :]) / c).value * dndz_fid
+
+        # Normalize the selection function so that \int_{0}^{inf} dr phi(r) = 1
+        phi_norm, error = quadrature(interp1d(grid.chi_array, phi_fid_array), grid.chi_min_int, grid.chi_max_int,
+                                     tol=1e-20, miniter=3000)
+        phi_perturbed_array *= phi_norm ** (-1)
+        phi_fid_array *= phi_norm ** (-1)
+
+        if get_delta_p:
+            super().__init__(phi_perturbed_array, phi_fid_array)
+        else:
+            self.phi_perturbed_array = phi_perturbed_array
+            self.phi_fid_array = phi_fid_array
+
 class gal_shear(Field):
-    def __init__(self, Template):
-        """ User provides a template of shifts to either the center or the width of a Gaussian dndz
-        - Inputs:
-            * shifts_template = 1D np.array of floats. A healpy map of shifts
-        """
+    def __init__(self, grid, sigma, z_mean, template_zmean_shifts=None, template_width_shifts=None):
+        self.sigma = sigma
+        self.z_mean = z_mean
+
+        g_d = gal_delta(grid, sigma, z_mean, grid, template_zmean_shifts, template_width_shifts, get_delta_p=False)
+        phi_fid_array = g_d.phi_fid_array
+        phi_perturbed_array = g_d.phi_perturbed_array
+
         # If doing cosmic shear, calculate the galaxy lensing kernel
-            # Dummy run to pre-compile with numba
-            utils.lens_efficiency_kernel(self.chi_array, self.chi_max_int, np.zeros_like(self.chi_array))
+        # Dummy run to pre-compile with numba
+        utils.lens_efficiency_kernel(grid.chi_array, grid.chi_max_int, np.zeros_like(grid.chi_array))
 
-            # Now in earnest
-            self.g_fid = 3 / 2. * Planck18.Om0 * Planck18.H0.value ** 2 / c.value ** 2 * self.chi_array * (
-                        1 + self.z_array[0, :]) * utils.lens_efficiency_kernel(self.chi_array, self.chi_max_int,
-                                                                               self.phi_fid_array)
-            self.g_pert = 3 / 2. * Planck18.Om0 * Planck18.H0.value ** 2 / c.value ** 2 * self.chi_array * (
-                        1 + self.z_array[0, :]) * utils.lens_efficiency_kernel(self.chi_array, self.chi_max_int,
-                                                                               self.phi_perturbed_array)
-        kernel = interp1d(self.chi_array, self.g_fid)
+        # Now in earnest
+        g_fid = 3 / 2. * Planck18.Om0 * Planck18.H0.value ** 2 / c.value ** 2 * grid.chi_array * (
+                1 + grid.z_array[0, :]) * utils.lens_efficiency_kernel(grid.chi_array, grid.chi_max_int, phi_fid_array)
+        g_pert = 3 / 2. * Planck18.Om0 * Planck18.H0.value ** 2 / c.value ** 2 * grid.chi_array * (
+                1 + grid.z_array[0, :]) * utils.lens_efficiency_kernel(grid.chi_array, grid.chi_max_int, phi_perturbed_array)
 
-        super().__init__()
+        super().__init__(grid, g_pert, g_fid)
